305cd693085c69fbf3c17f1a714d69ecf171b3fe
Grant Rettke <gcr@wisdomandwonder.com>
2019-04-13 21:26:02 -0500

    Font is wrong.

---
+++ .emacs.el
@@ -0,0 +1,6 @@
+;; Issue: Font is wrong.
+;; Doc: (emacs) Fonts
+(add-to-list 'default-frame-alist
+	     '(font . "Hack-15"))
+;; Note: Using GUI to set font not persisted.
+;; Note. M key is wrong.
