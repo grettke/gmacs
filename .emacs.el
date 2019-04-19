86a5a3ec0626685eec3a4258a3df14d1a8e4c854
Grant Rettke <gcr@wisdomandwonder.com>
2019-04-19 14:19:37 -0500

    Grammar

--- .emacs.el
+++ .emacs.el
@@ -15,8 +15,8 @@
 ;; Doc: (emacs) Fonts
 (add-to-list 'default-frame-alist
 	     '(font . "Hack-15"))
-;; - [X] Observation: Using GUI to set font not persisted.
-;; - [X] Note. M key is wrong.
+;; - [X] ouch: Using GUI to set font not persisted.
+;; - [X] ouch: M key is wrong.
 
 ;; Issue: Meta keys are wrong.
 ;; Doc: (emacs) Mac / GNUstep Basics
@@ -27,37 +27,37 @@
 (setq mac-option-modifier 'super)
 (setq mac-right-option-modifier 'none)
 (setq mac-function-modifier 'hyper)
-;; - [ ] Note: Commit to Git.
-;; - [X] Note: Keep files open.
-;; - [X] Note: Auto save buffer.
-;; - [X] Note: yes-or-no-p is too long.
+;; - [ ] need: Commit to Git.
+;; - [X] ouch: Keep files open.
+;; - [X] ouch: Auto save buffer.
+;; - [X] ouch: yes-or-no-p is too long.
 
 ;; Issue: yes-or-no-p is too long.
 ;; Doc: 5.8 Yes or No Prompts
 ;; Function is used for questions of actions with serious
 ;; consequences. So not changing anything.
-;; - [X] Note: Current column.
-;; - [ ] Note: Linewrap
+;; - [X] need: Current column.
+;; - [ ] need: Linewrap
 
 ;; Issue: Don't know current column.
 ;; Doc: (emacs) Mode Line
-;; - [X] Note: Size Indication mode 
-;; - [X] Note: Column Number
+;; - [X] disc: Size Indication mode 
+;; - [X] disc: Column Number
 
 ;; Issue: How big is this buffer?
 ;; Doc: (emacs) Optional Mode Line
 (size-indication-mode nil)
-;; - [X] Note: Line number mode
+;; - [X] need: Line number mode
 
 ;; Issue: What is the current line number?
 ;; Doc: (emacs) Optional Mode Line
 (line-number-mode nil)
-;; - [X] Note: Line numbers starts at 0, not 1
+;; - [X] disc: Line numbers starts at 0, not 1
 
 ;; Issue: Column starts at 0, not 1.
 ;; Doc: (emacs) Optional Mode Line
 (setq column-number-indicator-zero-based nil)
-;; - [X] Note: Other window frequent and hurts.
+;; - [X] ouch: Other window frequent and hurts.
 
 ;; Issue: Other window frequent and hurts.
 ;; Doc: Copy and paste a function definition.
@@ -74,14 +74,14 @@
 (desktop-save-mode 1)
 (setq desktop-restore-eager 5)
 (add-to-list 'desktop-clear-preserve-buffers ".emacs.el")
-;; - [X] Note: Minibuffer history
+;; - [X] need: Minibuffer history
 
 ;; Issue: Minibuffer history not saved.
 ;; Doc: (emacs) Saving Emacs Sessions
 ;; Use Customize to choose things to save. Save the options.
 ;; Move options to start of file.
 (savehist-mode nil)
-;; - [X] Note: Select buffer painful.
+;; - [X] ouch: Select buffer painful.
 
 ;; Issue: Select buffer painful.
 ;; Doc:	 Copy and paste a function definition.
